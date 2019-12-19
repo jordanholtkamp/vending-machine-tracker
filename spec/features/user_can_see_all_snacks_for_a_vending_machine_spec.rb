5fc2fa567dd2ffbdb5ab8a4b981cac53d9eb0371
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-19 09:46:53 -0700

    Add price to column to snack and add price to vending show page

--- spec/features/user_can_see_all_snacks_for_a_vending_machine_spec.rb
+++ spec/features/user_can_see_all_snacks_for_a_vending_machine_spec.rb
@@ -7,14 +7,19 @@ describe 'As a visitor' do
       dons  = sam.machines.create(location: "Don's Mixed Drinks")
       turing = sam.machines.create(location: "Turing Basement")
 
-      kitkat = Snack.create!(name: "Kit Kat")
-      chips = Snack.create!(name: 'Chips')
+      kitkat = Snack.create!(name: "Kit Kat", price: 1.50)
+      chips = Snack.create!(name: 'Chips', price: 2.00)
 
       dons.snacks << kitkat
       turing.snacks << kitkat
       turing.snacks << chips
 
-      
+      visit "/machines/#{dons.id}"
+
+      expect(page).to have_content(kitkat.name)
+      expect(page).to have_content(kitkat.price)
+      expect(page).to_not have_content(chips.name)
+      expect(page).to_not have_content(chips.price)
     end
   end
 end
